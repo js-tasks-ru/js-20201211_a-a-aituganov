65e1e4694c6b5283712646647ae5384e5d37463e
Askhat <lucciffeer@gmail.com>
2020-12-13 20:04:56 +0500

    2nd task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,15 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  let newArr = arr.slice();
+   let collator = new Intl.Collator(undefined, {
+     caseFirst: "upper"
+   });
+    
+   if (param === "asc") {
+     return newArr.sort((a, b) => collator.compare(a, b));
+   }
+   else if (param === "desc") {
+     return newArr.sort((a, b) => collator.compare(b, a));;
+   }
 }
